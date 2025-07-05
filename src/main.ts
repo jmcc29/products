c057b18ce9a8c20d888e81556d7d384b670ba300
José <jcuencacoronado@gmail.com>
2025-07-05 13:00:58 -0400

    Validaciones DTO class-validator y class-transformer en products

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Automatically remove properties that do not have decorators
+      forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are present
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,15 @@
-export class CreateProductDto {}
+import { IsNumber, IsOptional, IsString } from "class-validator";
+import { Type } from "class-transformer";
+
+export class CreateProductDto {
+    @IsString()
+    name: string;
+    
+    @IsString()
+    @IsOptional()
+    description?: string;
+    
+    @IsNumber()
+    @Type(() => Number)
+    price: number;
+}
